3f1f97a829150635e62f378cad3694bed1d11c8b
Andri Hadiansah <andrihadiansah@hotmail.com>
2025-07-31 04:32:45 +0700

    initial commit

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -5,8 +5,7 @@ export default function Page() {
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
         <h1 className="text-2xl font-bold">Hello World</h1>
-        <Button size="sm">Button Custom</Button>
-        <Button size="sm">Button andri</Button>
+        <Button size="sm">Button from Packages/ui</Button>
       </div>
     </div>
   );
